7895d00b1ed526f8537a903ba60f8d3d0d2c82ea
soutsos <106550353+Sidiro23@users.noreply.github.com>
2022-08-29 19:21:41 -0500

    started working on App.js/index.js

---
+++ src/components/About/index.js
@@ -0,0 +1,25 @@
+import React from 'react';
+
+function About(){
+  return(
+    <section className="container">
+            <h2 class="top-title">Savvas Sidiropoulos</h2>
+            <hr></hr>
+            <div>
+                <img class="mb-5" id="avatar" src="https://avatars.githubusercontent.com/u/106550353?s=400&u=93420fe10f0a22435675f62b3eef215d21c24bff&v=4" alt="Savvas Sidiropoulos" />
+
+                <p>
+                 
+                </p>
+                <p>
+                
+                </p>
+                <p>
+                
+                </p>
+            </div>
+        </section>
+  )
+}
+
+export default About;
